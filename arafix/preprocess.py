844068ddfda4a5060995683f2d7b85f94d48953a
Basma <92788812+Basma2423@users.noreply.github.com>
2025-07-22 20:36:48 +0300

    sukoon normalization

--- arafix/preprocess.py
+++ arafix/preprocess.py
@@ -21,6 +21,9 @@ def preprocess_keep_periods_and_commas(text, is_remove_last_diacritic=True):
   text = re.sub(r'[?؟!:]|[\n\t\r\f\v]+', '. ', text)
 
   text = text.replace("ٱ", "ا")
+  
+  # sukoon normalization
+  text = re.sub(r"[\u0616-\u061A\u06E1]", "\u0652", text)
 
   VALID_ARABIC_CHARS = LETTERS + DIACRITICS + NUMBERS + ENG_LETTERS + ENG_NUMBERS + [' ', '.', '.', ',', '،']
 
