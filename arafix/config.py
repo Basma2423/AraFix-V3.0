a8d995fbae3e8d03441f8318bb35f5de7747e379
Basma <92788812+Basma2423@users.noreply.github.com>
2025-07-22 17:50:58 +0300

    Add Eng letters and numbers

--- arafix/config.py
+++ arafix/config.py
@@ -21,6 +21,19 @@ DIACRITICS = [
     "ٍّ", # Shaddah + Kasratan
 ]
 
+ENG_LETTERS = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+NUMBERS = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+]
+
+ARABIC_NUMBERS = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩']
+
 ARAB_CHARS_NO_SPACE = "".join(LETTERS)
 
 MODEL_NAME = "CUAIStudents/AraFix-V3.0"
