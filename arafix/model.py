21b29cf7ba2298e257ffb3bb78e49b75d4bad059
Basma <92788812+Basma2423@users.noreply.github.com>
2025-08-16 20:45:08 +0300

    Add tokenize method

--- arafix/model.py
+++ arafix/model.py
@@ -18,16 +18,25 @@ class AraFixModel:
             self._tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
         return self._tokenizer
     
-    def predict(self, text):
+    def tokenize(self, text, **kwargs):
+        inputs = self.tokenizer(
+            text,
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            **kwargs
+        )
         
+        if "token_type_ids" in inputs:
+            del inputs["token_type_ids"]
+        
+        return inputs
+
+    def predict(self, text):
         if not text:
             return None
         
-        inputs = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-
-        if 'token_type_ids' in inputs:
-            del inputs['token_type_ids']
-
+        inputs = self.tokenize(text)
         outputs = self.model.generate(**inputs, max_new_tokens=128)
         decoded_output = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         return decoded_output
