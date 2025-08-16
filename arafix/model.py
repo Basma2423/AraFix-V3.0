ed5eace1ef25f3dc6314e140d48a100fc90a9043
Basma <92788812+Basma2423@users.noreply.github.com>
2025-08-16 20:51:59 +0300

    Pass truncation and padding param to tokenize function

--- arafix/model.py
+++ arafix/model.py
@@ -18,24 +18,21 @@ class AraFixModel:
             self._tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
         return self._tokenizer
     
-    def tokenize(self, text, **kwargs):
+    def tokenize(self, text, padding=True, truncation=True, **kwargs):
         inputs = self.tokenizer(
             text,
             return_tensors="pt",
-            padding=True,
-            truncation=True,
+            padding=padding,
+            truncation=truncation,
             **kwargs
         )
-        
         if "token_type_ids" in inputs:
             del inputs["token_type_ids"]
-        
         return inputs
 
     def predict(self, text):
         if not text:
             return None
-        
         inputs = self.tokenize(text)
         outputs = self.model.generate(**inputs, max_new_tokens=128)
         decoded_output = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
